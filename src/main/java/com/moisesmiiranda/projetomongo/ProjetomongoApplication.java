3ec35eb60ea15cf28d27afc8f4ee09c044f3bf68
moisesmiiranda <moisesmiranda1994@gmail.com>
2021-02-23 08:44:32 -0300

    Commit inicial, projeto criado.

---
+++ src/main/java/com/moisesmiiranda/projetomongo/ProjetomongoApplication.java
@@ -0,0 +1,13 @@
+package com.moisesmiiranda.projetomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomongoApplication.class, args);
+	}
+
+}
