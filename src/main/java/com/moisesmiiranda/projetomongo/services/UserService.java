2ce1867b1b3fc1fc767f9959c4640e2e521889fc
moisesmiiranda <moisesmiranda1994@gmail.com>
2021-02-24 11:02:31 -0300

    Obtendo um usuario por id

--- src/main/java/com/moisesmiiranda/projetomongo/services/UserService.java
+++ src/main/java/com/moisesmiiranda/projetomongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.moisesmiiranda.projetomongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.moisesmiiranda.projetomongo.domain.User;
 import com.moisesmiiranda.projetomongo.repository.UserRepository;
+import com.moisesmiiranda.projetomongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {//serviço
@@ -15,5 +17,10 @@ public class UserService {//serviço
 	private UserRepository repo;//acessa o repositório
 	public List <User> findALL(){ //retorna todos os usuarios
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
