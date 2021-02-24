82dc4cea4ac8dbe3fed8e6aadebb4d0fa50f587f
moisesmiiranda <moisesmiranda1994@gmail.com>
2021-02-24 20:18:25 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/moisesmiiranda/projetomongo/config/Instantiation.java
+++ src/main/java/com/moisesmiiranda/projetomongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.moisesmiiranda.projetomongo.domain.Post;
 import com.moisesmiiranda.projetomongo.domain.User;
 import com.moisesmiiranda.projetomongo.dto.AuthorDTO;
+import com.moisesmiiranda.projetomongo.dto.CommentDTO;
 import com.moisesmiiranda.projetomongo.repository.PostRepository;
 import com.moisesmiiranda.projetomongo.repository.UserRepository;
 //classe responsável pela carga inicial do banco de dados
@@ -43,6 +44,13 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null,sdf.parse("21/02/2021"), "Partiu viagem","Indo para Chicaco, Abraços!",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/02/2021"), "Bom dia!","Lindo dia hoje!",new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		//fazendo associações aos posts
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		//salvando posts
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
--- src/main/java/com/moisesmiiranda/projetomongo/domain/Post.java
+++ src/main/java/com/moisesmiiranda/projetomongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.moisesmiiranda.projetomongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.moisesmiiranda.projetomongo.dto.AuthorDTO;
+import com.moisesmiiranda.projetomongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String boby;
 	private AuthorDTO author;
 
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 
 	}
@@ -74,6 +79,16 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -98,7 +113,6 @@ public class Post implements Serializable {
 			return false;
 		return true;
 	}
-
 
 
 }
